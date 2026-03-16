5fc5a0be97d32c4edaaf6b157a860c6d7f66251d
ndu <OkomaNdu>
2026-03-15 23:28:08 -0400

    add ec2 deployment config

--- entry-script.sh
+++ entry-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo yum update -y && sudo yum install docker -y 
 sudo systemctl start docker
+sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
-docker run -p 8080:80 nginx
+sudo docker run -d -p 8080:80 nginx
+
